0f2f813a8c95019c3582be2d423347809b03c378
Niko Kivel <niko.kivel@lightsource.ca>
2024-05-06 23:17:17 -0600

    silly example

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from room4improvement import main as r2d2
 CRED = '\033[91m'                       
 CEND = '\033[0m'
     
+
+def a_classic(example: int) -> None:
+    """Takes a number and returns the square."""  # noqa: D401
+    exmaple = example**2
+    return (example)
+
 if __name__ == "__main__":
    	print("-"*80)
    	print(f"{CRED}This line has/had a TAB indentation.{CEND}")
