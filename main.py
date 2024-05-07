87bc51b0fef7d37ab5ed7a29362418584725cf40
Niko Kivel <niko.kivel@lightsource.ca>
2024-05-06 18:44:41 -0600

    more errors for main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import sys
 
 from room4improvement import main as r2d2
 
-CRED = '\033[91m'
+CRED = '\033[91m'                       
 CEND = '\033[0m'
-
+    
 if __name__ == "__main__":
    	print("-"*80)
    	print(f"{CRED}This line has/had a TAB indentation.{CEND}")
