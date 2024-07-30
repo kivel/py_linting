d51839d9676101d17745daf9fec2ac4f39840b6e
Niko Kivel <niko.kivel@lightsource.ca>
2024-07-30 13:51:24 -0600

    fixed all complains

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
-import sys
-
 from room4improvement import main as r2d2
 
-CRED = '\033[91m'                       
-CEND = '\033[0m'
-    
+CRED = "\033[91m"
+CEND = "\033[0m"
 
-def a_classic(example: int) -> None:
-    """Takes a number and returns the square.
+
+def a_classic(example: int) -> int:
+    """
+    Takes a number and returns the square.
+
     This is broken _and_ dead code, it migth fall prey to the vultre.
     But, Connor is the expert.
     """  # noqa: D401
-    exmaple = example**2
-    return (example)
+    return example**2
+
 
 if __name__ == "__main__":
-   	print("-"*80)
-   	print(f"{CRED}This line has/had a TAB indentation.{CEND}")
-   	r2d2()
-   	print("-"*80)
+    print("-" * 80)
+    print(f"{CRED}This line has/had a TAB indentation.{CEND}")
+    r2d2()
+    print("-" * 80)
